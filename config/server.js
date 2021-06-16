e27d12ccd730897c52404406d054fd4b829614b9
Jyoti <jyoti.nambiar@medieinstitutet.se>
2021-06-16 09:29:14 +0200

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4c378f2432c6931a5595223088ddd5b8'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'bb3f39c0-df66-4613-9b60-cb88454f6634'
+};
